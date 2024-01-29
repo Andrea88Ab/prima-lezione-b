c22852660252bd999151ee75282e5790bf2de845
Andrea88Ab <andrea88ab@gmail.com>
2024-01-29 15:17:08 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace prima_lezione_b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    
+
+}
